9d47b9f3016aa8b5dc8da966288be29ce0f83ce4
Keviindran Ramachandran <keviinx@yahoo.com>
2021-07-23 23:47:40 +0200

    done until Keyboard Input Controls & Key Pressed Event

--- SpaceInvader/main.py
+++ SpaceInvader/main.py
@@ -15,6 +15,7 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('space-invaders.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
@@ -30,5 +31,19 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        # if keystroke is pressed checked whether its right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    # 5 = 5 + -0.1 -> 5 - 0.1
+    # 5 = 5 + 0.1
+
+    playerX += playerX_change
     player(playerX, playerY)
     pygame.display.update()
