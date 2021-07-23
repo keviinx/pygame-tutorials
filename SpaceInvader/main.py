ad419e653f8897737e8978dd22c4669e7cd91db0
Keviindran Ramachandran <keviinx@yahoo.com>
2021-07-23 21:49:05 +0200

    done until Changing the Title, Logo and Background Color

--- SpaceInvader/main.py
+++ SpaceInvader/main.py
@@ -6,9 +6,18 @@ pygame.init()
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
 # Game Loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # RGB - Red, Green, Blue
+    screen.fill((0, 0, 0))
+    pygame.display.update()
