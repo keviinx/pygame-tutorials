bcf0ef77787030aba74c8518ade77f4a98117088
Keviindran Ramachandran <keviinx@yahoo.com>
2021-07-23 23:20:57 +0200

    done until Adding Images into Our Space Invader Game

--- SpaceInvader/main.py
+++ SpaceInvader/main.py
@@ -11,13 +11,24 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load('space-invaders.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
 # Game Loop
 running = True
 while running:
+
+    # RGB - Red, Green, Blue
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # RGB - Red, Green, Blue
-    screen.fill((0, 0, 0))
+    player()
     pygame.display.update()
