c838485dab6c5b412f37cb0d0fcd9eb611066faa
Keviindran Ramachandran <keviinx@yahoo.com>
2021-07-25 21:54:43 +0200

    done until Shooting Multiple Bullets at Space Invaders

--- SpaceInvader/main.py
+++ SpaceInvader/main.py
@@ -70,7 +70,9 @@ while running:
             if event.key == pygame.K_RIGHT:
                 playerX_change = 5
             if event.key == pygame.K_SPACE:
-                fire_bullet(playerX, bulletY)
+                if bullet_state is "ready":
+                    bulletX = playerX
+                    fire_bullet(bulletX, bulletY)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -97,9 +99,13 @@ while running:
         enemyX_change = -4
         enemyY += enemyY_change
 
-    # Bullet Movement
+    # Bullet 
+    if bulletY <= 0:
+        bulletY = 480
+        bullet_state = "ready"
+
     if bullet_state is "fire":
-        fire_bullet(playerX, bulletY)
+        fire_bullet(bulletX, bulletY)
         bulletY -= bulletY_change
 
     player(playerX, playerY)
